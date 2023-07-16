951d91a4535bffeb751423050be39f9de7d620b2
Kobi Pollak <kobipollak@gmail.com>
2023-07-16 18:42:17 +0300

    Library files

---
+++ Client/project_7/src/App.js
@@ -0,0 +1,49 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  redirect as Redirect,
+  Navigate,
+} from "react-router-dom";
+
+import NotFound from "./components/notFound";
+import SignIn from "./components/signIn";
+import SignUp from "./components/logUp";
+import Application from "./components/application";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signIn" replace />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/application/:id" element={<Application />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div> */}
+    </>
+  );
+}
+
+export default App;
