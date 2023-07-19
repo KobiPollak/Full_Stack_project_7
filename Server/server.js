db90fd3e6dc07da440b921ff75ab0ac60b6fa63c
Kobi Pollak <kobipollak@gmail.com>
2023-07-19 10:34:45 +0300

    adding DB information to .env file

--- Server/server.js
+++ Server/server.js
@@ -11,17 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Kobi09pollak",
-  database: "project7",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 var poll = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Kobi09pollak",
-  database: "project7",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 function authenticateToken(req, res, next) {
